4d41c50296904b4f1f3d940befaabb8d53cdfa51
Danielhsuuu <danielhsu0614@gmail.com>
2022-03-25 14:33:29 +0800

    updated db connection

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 //connect to database
 try {
     $conn = new PDO(
-        "mysql:host=localhost;dbname=photo_sharing_app",
-        "root",
-        "Am03zw5jGnsdtElS"
+        "mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_f0db736d53d1f30",
+        "bc613a7128a228",
+        "c11e1770"
     );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
